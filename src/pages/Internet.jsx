ced1a4f62543e0753e0903f0cc76f07959c7ab62
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-06-03 10:04:37 -0700

    Valores reales en Internet

--- src/pages/Internet.jsx
+++ src/pages/Internet.jsx
@@ -9,28 +9,19 @@ export default function Page_Internet() {
                 <div className="row">
                     <Card
                         titulo="Inicial"
-                        subtitulo="Internet 20 megas"
-                        precio="$459"
-                        velbajada="20"
-                        velsubida="3"
-                        descripcion="Descuento de por vida"
-                        descripcion2="-$20 a partir del 6º mes"
-                    />
-                    <Card
-                        titulo="Inicial"
-                        subtitulo="Internet 40 megas"
-                        precio="$459"
-                        velbajada="20"
-                        velsubida="3"
+                        subtitulo="Internet 50 megas"
+                        precio="$469"
+                        velbajada="50"
+                        velsubida="5"
                         descripcion="Descuento de por vida"
                         descripcion2="-$20 a partir del 6º mes"
                     />
                     <Card
                         titulo="Inicial"
                         subtitulo="Internet 100 megas"
-                        precio="$459"
-                        velbajada="20"
-                        velsubida="3"
+                        precio="$569"
+                        velbajada="100"
+                        velsubida="10"
                         descripcion="Descuento de por vida"
                         descripcion2="-$20 a partir del 6º mes"
                     />
@@ -38,29 +29,29 @@ export default function Page_Internet() {
                     <Card
                         titulo="Inicial"
                         subtitulo="Internet 200 megas"
-                        precio="$459"
-                        velbajada="20"
-                        velsubida="3"
+                        precio="$689"
+                        velbajada="200"
+                        velsubida="20"
                         descripcion="Descuento de por vida"
-                        descripcion2="-$20 a partir del 6º mes"
+                        descripcion2="-$40 a partir del 6º mes"
                     />
                     <Card
                         titulo="Inicial"
                         subtitulo="Internet 500 megas"
-                        precio="$459"
-                        velbajada="20"
-                        velsubida="3"
+                        precio="$899"
+                        velbajada="500"
+                        velsubida="50"
                         descripcion="Descuento de por vida"
-                        descripcion2="-$20 a partir del 6º mes"
+                        descripcion2="-$65 a partir del 6º mes"
                     />
                     <Card
                         titulo="Inicial"
                         subtitulo="Internet 1000 megas"
-                        precio="$459"
-                        velbajada="20"
-                        velsubida="3"
+                        precio="$1599"
+                        velbajada="1000"
+                        velsubida="100"
                         descripcion="Descuento de por vida"
-                        descripcion2="-$20 a partir del 6º mes"
+                        descripcion2="-$80 a partir del 6º mes"
                     />
                 </div>
             </div>
